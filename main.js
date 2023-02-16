fbcadbae8adfb1850c954d5da822eb25560addc1
Liasnik <108823272+Liasnik@users.noreply.github.com>
2023-02-16 15:18:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,209 +1,139 @@
-const button = document.querySelector('.butt-1')
-.addEventListener('click', myClick);
+const butt_replase = document.querySelector('.butt-3-repl');
+butt_replase.addEventListener('click', replaceName);
 
-const button2 = document.querySelector('.butt-2')
-.addEventListener('click', myClick2_1);
+const button1_1 = document.querySelector('.butt-1-1');
+button1_1.addEventListener('click', myClick2_2);
+button1_1.addEventListener('click', myClick3_1);
 
-const button3 = document.querySelector('.butt-4')
-.addEventListener('click', myClick3_1);
+const button1 = document.querySelector('.butt-1');
+button1.addEventListener('click', myClick);
+
+const button2 = document.querySelector('.butt-2');
+button2.addEventListener('click', myClick2_2);
+
+const button3 = document.querySelector('.butt-4');
+button3.addEventListener('click', myClick3_1);
+
+const input = document.querySelector('.text-3');
+input.value = JSON.parse(localStorage.getItem('key'));
+
+const inputReplace = document.querySelector('.text-3-replace');
+inputReplace.value = JSON.parse(localStorage.getItem('key2'));
+
+const inList = document.querySelector('.text-1');
+inList.value = JSON.parse(localStorage.getItem('text'));
+
+function arrKey() {
+    const keyWord = document.querySelector('.text-3').value;
+
+    if (!keyWord) {
+        alert('Введи ключевые слова!');
+        return
+    } 
+    
+    localStorage.setItem('key', JSON.stringify(keyWord));
+    
+    const arrayKeywords = keyWord.split(' ');
+
+ return arrayKeywords;
+}
+
+function arrText() {
+    const inText = document.querySelector('.text-1').value;
+    
+    localStorage.setItem('text', JSON.stringify(inText));
+
+    const arrayFromString = inText.split('\n');
+    const uniqArr = [...new Set(arrayFromString)];
+
+     return uniqArr;
+}
 
 function myClick() {
-    const keyWord = document.querySelector('.text-3').value; 
-    const inText = document.querySelector('.text-1').value;  
-    
-    const arrayFromString = inText.split('\n');
+    const arrayKeywords = arrKey(); 
+    const uniqArray = arrText();
 
-    const withUsers = arrayFromString.filter( item => {    
+    const newArray = uniqArray.filter(item => { 
+        return arrayKeywords.some(allowed => {
+         return item.includes(`${allowed}\t`)
+        })
+  });
+
+     const listWith = newArray.join('\n'); 
      
-        return item.includes(`${keyWord}\t`);
-    });
+    document.querySelector('.text-2').value = `${listWith}\n` 
 
-    let mySet = new Set(withUsers)
-    let newArrey = [...mySet]
+    const newArray2 = uniqArray.filter(item => { 
+        return !arrayKeywords.some(forbidden => {
+         return item.includes(`${forbidden}\t`)
+        })
+  });
+      
+    const listWithout = newArray2.join('\n');
 
-    const listWith = newArrey.join('\n') 
-    
-    // const listWith = withUsers.join('\n') 
-    document.querySelector('.text-2').value += `${listWith}\n` 
-
-    console.log(arrayFromString);
-    console.log(listWith);
-    console.log(`${listWith}\n`);
-    
-    const withoutUsers = arrayFromString.filter( item => {    
-     
-        return !item.includes(`${keyWord}\t`);
-    });
-
-    let mySet2 = new Set(withoutUsers)
-    let newArrey2 = [...mySet2]
-
-    const listWithout = newArrey2.join('\n')
-
-     document.querySelector('.text-4').value = listWithout
+     document.querySelector('.text-4').value = listWithout;
 };
 
-// function myClick2() {
-//     const keyWord = document.querySelector('.text-3').value;
-//     const keyWord2 = document.querySelector('.text-3-2').value;
-//     const keyWord3 = document.querySelector('.text-3-3').value;
-//     const keyWord4 = document.querySelector('.text-3-4').value;
-//     const keyWord5 = document.querySelector('.text-3-5').value;
-//     const keyWord6 = document.querySelector('.text-3-6').value;
-//     const keyWord7 = document.querySelector('.text-3-7').value;
-//     const keyWord8 = document.querySelector('.text-3-8').value;
-//     const keyWord9 = document.querySelector('.text-3-9').value;
-//     const keyWord10 = document.querySelector('.text-3-10').value;
-//     const keyWord11 = document.querySelector('.text-3-11').value;
-//     const keyWord12 = document.querySelector('.text-3-12').value;
-//     const keyWord13= document.querySelector('.text-3-13').value;
-//     const keyWord14 = document.querySelector('.text-3-14').value;
-
-//     const inText = document.querySelector('.text-1').value;  
-//     const arrayFromString = inText.split('\n');
-
-//     const withUsers = arrayFromString.filter( item => {    
-      
-//         if (item.includes(keyWord)) {
-//         return item};
-
-//         if (item.includes(keyWord2)) {
-//             return item};
-
-//         if (item.includes(keyWord3)) {
-//             return item};
-
-//         if (item.includes(keyWord4)) {
-//             return item};
-            
-//         if (item.includes(keyWord5)) {
-//             return item};
-            
-//         if (item.includes(keyWord6)) {
-//             return item};
-
-//         if (item.includes(keyWord7)) {
-//             return item};
-            
-//         if (item.includes(keyWord8)) {
-//             return item};
-
-//         if (item.includes(keyWord9)) {
-//             return item};
-
-//         if (item.includes(keyWord10)) {
-//             return item};
-            
-//         if (item.includes(keyWord11)) {
-//             return item};  
-            
-//         if (item.includes(keyWord12)) {
-//             return item};
-
-//         if (item.includes(keyWord13)) {
-//             return item};
-            
-//         if (item.includes(keyWord14)) {
-//             return item};   
-// });
-
-//         let mySet = new Set(withUsers)
-//         let newArrey = [...mySet]
-
-//         const listWith = newArrey.join('\n')
+function myClick2_2() {
+    const arrayKeywords = arrKey(); 
+    const uniqArray = arrText();
     
-//     document.querySelector('.text-2').value = `${listWith}\n`
-// };
-
-// function myClick3() {
-//     const keyWord = document.querySelector('.text-3').value;
-//     const inText = document.querySelector('.text-1').value;
-
-//     const arrayFromString = inText.split('\n');
-
-//     const withoutUsers = arrayFromString.filter( item => {    
-      
-//         if ( !item.includes(keyWord)) {
-//             return item;
-//         }         
-//     })
-
-//     let mySet2 = new Set(withoutUsers)
-//     let newArrey2 = [...mySet2]
-
-//     const listWithout = newArrey2.join('\n')
-
-//      document.querySelector('.text-4').value = listWithout;
-// };
-
-function myClick2_1() {
-    const keyWord = document.querySelector('.text-3').value;
-    const arrayKeywords = keyWord.split(' ')
-    
-    console.log(arrayKeywords);
-
-    const inText = document.querySelector('.text-1').value;  
-    const arrayFromString = inText.split('\n');
-
-    console.log(arrayFromString);
-  
-    let result = [] 
-
-    arrayFromString.filter( item => { 
-           
-      for (let i = 0; i < arrayKeywords.length; i++ ) {
-         
-        if (item.includes(arrayKeywords[i])) {
-            result.push(item)
-        }
-      }
-        return result
-      });
-
-      console.log(result);
-
-      let mySet = new Set(result)
-      let newArrey = [...mySet]
-
-     const listWithout = newArrey.join('\n'); 
-    document.querySelector('.text-2').value = listWithout;
+    const newArray = uniqArray.filter(item => { 
+          return arrayKeywords.some(allowed => {
+           return item.includes(allowed)
+          })
+    });
+        
+     const listWithout = newArray.join('\n'); 
+     document.querySelector('.text-2').value = listWithout;
 }
 
 function myClick3_1() {
-    const keyWord = document.querySelector('.text-3').value;
-    const arrayKeywords = keyWord.split(' ')
+    const arrayKeywords = arrKey(); 
+    const uniqArray = arrText();
 
-    //console.log(typeof arrayKeywords);
-    //console.log(arrayKeywords);
-
-    const inText = document.querySelector('.text-1').value;  
-    const arrayFromString = inText.split('\n');
-
-    console.log(arrayFromString);
-  
-    let result = [] 
-
-    arrayFromString.filter( item => { 
-           
-      for (let i = 0; i < arrayKeywords.length; i++ ) {
-         
-        if (item.includes(arrayKeywords[i])) {
-            result.push(item)
-        }       
-       
-        // console.log(result);
-        // console.log(item);
-        // console.log(arrayKeywords[i]);
-        }
-
-        return result
-      });
-   
-      console.log(result);
-   
-      let mySet = new Set(result)
-      let newArrey = [...mySet]
-      
+    const newArrey = uniqArray.filter(item => { 
+          return !arrayKeywords.some(forbidden => {
+           return item.includes(forbidden)
+          })
+    });
+ 
      const listWithout = newArrey.join('\n'); 
-    document.querySelector('.text-4').value = listWithout;
+     document.querySelector('.text-4').value = listWithout;
 }
+
+function replaceName() {
+    const arrayKeywords = arrKey(); 
+    const uniqArray = arrText();
+
+    const newWord = document.querySelector('.text-3-replace').value;
+
+    if (!newWord) {
+        alert('Введи ключевые слова!');
+        return
+    } 
+    
+    localStorage.setItem('key2', JSON.stringify(newWord));
+    
+    const arrayNewWords = newWord.split(' ');
+
+function replaceName(array, targetArr, replacementArr) {
+    for (let i = 0; i < targetArr.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+         array[j] = array[j].replace(`${targetArr[i]}\t`,
+          `${replacementArr[i]}\t`);
+        }
+    }
+    return array;
+}
+
+const newArr = replaceName(uniqArray, arrayKeywords, arrayNewWords);
+const newArray = newArr.join('\n');
+
+    document.querySelector('.text-2').value = newArray;
+}
+
+document.getElementById('del').onclick = function() {
+    localStorage.clear('key');
+ }  
+
